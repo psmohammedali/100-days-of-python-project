4de8d34e620b917d3409d67f906b12233a36e196
Mohammed Ali P S <psmohammedali4@gmail.com>
2022-10-11 13:47:42 +0530

    Day-1 completed

---
+++ Day-1/project_1.py
@@ -0,0 +1,4 @@
+print("Welcome to Ali's Band Generator.")
+my_name = input("Enter your city that you currently live in:")
+pet_name = input("Enter your pet's name: ")
+print("Horrey!!, your Band name is",my_name + " " + pet_name)
